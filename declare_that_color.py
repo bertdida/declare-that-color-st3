57d7bca50405323ca0ea34318ab15afc59507c45
Herbert Verdida <hjverdida@gmail.com>
2019-03-08 09:09:11 +0800

    Load settings

--- declare_that_color.py
+++ declare_that_color.py
@@ -3,22 +3,25 @@ import sublime_plugin
 from .dtc import CSS
 from .dtc import CSSExtension
 
+SETTINGS_FILE = 'declare_that_color.sublime-settings'
+
 
 class DeclareThatColor(sublime_plugin.TextCommand):
     def __init__(self, view):
         self.view = view
 
+        settings = sublime.load_settings(SETTINGS_FILE)
+        self.css_selector = settings.get('css_selector')
+        self.css_extension = settings.get('css_extension')
+
     def run(self, edit):
         region = sublime.Region(0, self.view.size())
         file_content = self.view.substr(region)
 
-        css_selector = ':root'
-        css_extension = 'scss'
-
         if False:
-            css = CSS(css=file_content, selector=css_selector)
+            css = CSS(css=file_content, selector=self.css_selector)
         else:
-            css = CSSExtension(css=file_content, language=css_extension)
+            css = CSSExtension(css=file_content, language=self.css_extension)
 
         css.save_previous_declarations()
         css.remove_declarations()
