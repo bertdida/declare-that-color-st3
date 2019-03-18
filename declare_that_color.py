726e51e80650db2b85826d8e41b5569a4ef0cdce
Herbert Verdida <hjverdida@gmail.com>
2019-03-18 10:43:33 +0800

    Code refactoring

--- declare_that_color.py
+++ declare_that_color.py
@@ -15,26 +15,36 @@ class DeclareThatColor(sublime_plugin.TextCommand):
         self.css_selector = settings.get('css_selector')
         self.css_preprocessor = settings.get('css_preprocessor')
 
-        if Preprocessor.is_supported(self.css_preprocessor):
+        if self.is_preprocessor_supported(self.css_preprocessor):
             self.css = Preprocessor(self.css_preprocessor)
         else:
             self.css = Vanilla(self.css_selector)
 
+    @staticmethod
+    def is_preprocessor_supported(preprocessor):
+
+        return preprocessor is not None \
+            and Preprocessor.is_supported(preprocessor)
+
     def run(self, edit):
 
-        region = sublime.Region(0, self.view.size())
-        buffer_ = self.view.substr(region)
+        region, buffer_ = self.get_region_and_buffer()
 
         self.view.replace(
             edit, region, self.css.declare_hexcodes(buffer_))
+
+    def get_region_and_buffer(self):
+
+        region = sublime.Region(0, self.view.size())
+
+        return tuple(region, self.view.substr(region))
 
 
 class UndeclareThatColor(DeclareThatColor):
 
     def run(self, edit):
 
-        region = sublime.Region(0, self.view.size())
-        buffer_ = self.view.substr(region)
+        region, buffer_ = self.get_region_and_buffer()
 
         self.view.replace(
             edit, region, self.css.undeclare_hexcodes(buffer_))
